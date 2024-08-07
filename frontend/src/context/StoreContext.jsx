220fcdaa6518cf4825461484047463690ff55d4e
sankeernaduraii <sankeernasn@gmail.com>
2024-08-07 12:00:01 +0530

    subtotal is added in cart file

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -17,16 +17,31 @@ const StoreContextProvider = (props) => {
     const removeFromCart=(itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
-    useEffect(()=>
+const getTotalCartAmount=()=>{
+  let totalAmount=0;
+  for(const item in cartItems)
+  {
+    if(cartItems[item]>0)
     {
-        console.log(cartItems);
-    },[cartItems])
+    let itemInfo=food_list.find((product)=>product._id===item);
+    totalAmount+=itemInfo.price*cartItems[item];
+    }
+
+  }
+  return totalAmount;
+}
+
+    // useEffect(()=>
+    // {
+    //     console.log(cartItems);
+    // },[cartItems])
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
-    removeFromCart
+    removeFromCart,
+    getTotalCartAmount
   };
 
   return (
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css';
 import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -44,7 +44,7 @@ const Cart = () => {
         <div>
         <div className='cart-total-details'>
           <p>Subtotal</p>
-          <p>{0}</p> {/* Placeholder for subtotal */}
+          <p>{getTotalCartAmount}</p> {/* Placeholder for subtotal */}
         </div>
         <hr />
         <div className='cart-total-details'>
@@ -54,7 +54,7 @@ const Cart = () => {
         <hr />
         <div className='cart-total-details'>
           <p>Total</p>
-          <p>{0}</p> {/* Placeholder for total */}
+          <p>{getTotalCartAmount()+2}</p> {/* Placeholder for total */}
         </div>
         </div>
         <button>PROCEED TO CHECKOUT</button>
