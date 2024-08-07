91db1cf37ab521c4a6fc2d7e4d7fe317400a2f4f
sankeernaduraii <sankeernasn@gmail.com>
2024-08-07 14:00:59 +0530

    order page updated

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import'./PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 const PlaceOrder = () => {
+  const{getTotalCartAmount}=useContext(StoreContext);
   return (
     <>
-    <form className='place-order'>
-      <div className='place-order-left'>
-      
-      </div>
+      <form className='place-order'>
+        <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+        <div className='multi-fields'>
+          <input type="text" placeholder='First name'/>
+          <input type='text' placeholder='Last name'/>
+        </div>
+        <input type="email" placeholder='Email address'/>
+        <input type='text' placeholder='Street'/>
+        </div>
+        <div className='multi-fields'>
+          <input type="text" placeholder='City'/>
+          <input type='text' placeholder='State'/>
+        </div>
+        <div className='multi-fields'>
+          <input type="text" placeholder='Zip Code'/>
+          <input type='text' placeholder='Country'/>
+        </div>
+        <input type='text' placeholder='phone'/>
+        
 
-    </form>
-    <div>
       
-    </div>
-    </>
-  )
-}
+      <div className='place-order-right'>
+      <div className='cart-total'>
+          <h2>Cart Totals</h2>
+        <div>
+        <div className='cart-total-details'>
+          <p>Subtotal</p>
+          <p>${getTotalCartAmount()}</p> {/* Placeholder for subtotal */}
+        </div>
+        <hr />
+        <div className='cart-total-details'>
+          <p>Delivery Fee</p>
+          <p>${45}</p> {/* Placeholder for delivery fee */}
+        </div>
+        <hr />
+        <div className='cart-total-details'>
+          <p>Total</p>
+          <p>${getTotalCartAmount()+45}</p> {/* Placeholder for total */}
+        </div>
+        </div>
+        <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+      </div>
+      </form>
+   </>
+    )
+  }
 
 export default PlaceOrder
